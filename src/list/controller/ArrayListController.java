238b86cec61beb5f9cd214ed0ba6e5ffac7e4dab
abro5723 <abro5723@740s-pl-114.local>
2015-10-07 09:54:43 -0600

    Making the code actually wrk

--- src/list/controller/ArrayListController.java
+++ src/list/controller/ArrayListController.java
@@ -1,11 +1,31 @@
 package list.controller;
 
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 public class ArrayListController
 {
+	private ArrayList<String> firstWords;
+	
+	public ArrayListController()
+	{
+		firstWords = new ArrayList<String>();
+	}
 	
 	public void start()
 	{
+		firstWords.add("gfsageragfdabar");
 		
+		firstWords.add("babababababa");
+	}
+	
+	private void showList()
+	{
+		for(int spot=0; spot < firstWords.size(); spot++)
+		{
+			JOptionPane.showMessageDialog(null, firstWords.get(spot) + " is at spot# " + spot);
+		}
 	}
 
 }
